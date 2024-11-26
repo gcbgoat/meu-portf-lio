5387e6219e382e3169de81ac1a4709fde9807a2d
guilhermebaltar <gcb@cesar.school>
2024-11-26 19:50:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Página carregada!');
+let likeCount = 0;
+
+
+const likeButton = document.getElementById('likeButton');
+const unlikeButton = document.getElementById('unlikeButton');
+const likeCountElement = document.getElementById('likeCount');
+
+
+likeButton.addEventListener('click', function() {
+    likeCount++; 
+    likeCountElement.textContent = `Curtidas: ${likeCount}`; 
+
+   
+    likeButton.style.display = 'none';
+    unlikeButton.style.display = 'inline-block';
+});
+
+
+unlikeButton.addEventListener('click', function() {
+    likeCount--; 
+    likeCountElement.textContent = `Curtidas: ${likeCount}`;
+
+    likeButton.style.display = 'inline-block';
+    unlikeButton.style.display = 'none';
 });
